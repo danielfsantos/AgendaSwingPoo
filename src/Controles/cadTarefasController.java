bf66376647aa67f5c7fd47c47a8b1778d8347652
Daniel <danielfsantos56@gmail.com>
2019-05-11 15:16:07 -0300

    Initial Commit

---
+++ src/Controles/cadTarefasController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controles;
+
+/**
+ *
+ * @author Daniel
+ */
+public class cadTarefasController {
+
+    public boolean CampoIsNull(String campo1, String campo2, String campo3) {
+        if (campo1 == "" & campo2 == "" && campo3 == "") {
+
+            return true;
+        } else {
+
+            return false;
+        }
+    }
+
+}
