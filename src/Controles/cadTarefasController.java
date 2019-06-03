8fb9536c0d4fee6278d7855cc21ccb9c321ef458
Daniel <danielfsantos56@gmail.com>
2019-06-03 00:30:46 -0300

    Ajudando o Daniel

--- src/Controles/cadTarefasController.java
+++ src/Controles/cadTarefasController.java
@@ -4,26 +4,13 @@
  * and open the template in the editor.
  */
 package Controles;
-
-/**
- *
- * @author Daniel
- */
-<<<<<<< HEAD
-public class cadTarefasController {
-    
-=======
->>>>>>> danielguimaraes
-
 import Dados.TarefaConnection;
 import java.sql.Date;
 import java.sql.SQLException;
 
 public class cadTarefasController extends TarefaConnection {
-    
-    public static void getSaveTarefa(String nome,Date datatarefa,String descricao,int id_cad_status,int id_tipomateria,int id_tipotarefa) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+    public static void getSaveTarefa(String nome,Date datatarefa,String descricao,String id_cad_status,String id_tipomateria,String id_tipotarefa) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         TarefaConnection.saveTarefa(nome,datatarefa,descricao,id_cad_status,id_tipomateria,id_tipotarefa);
-        
     }
  }
 
